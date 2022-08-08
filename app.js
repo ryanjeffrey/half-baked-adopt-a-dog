ea698de9afb63c271c837fbb1ce6766b22dc54f2
ryanjeffrey <ryanjeffreysmith@gmail.com>
2022-08-08 18:43:12 -0500

    Use async function to receive and load fetched dog data

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ import { renderDogCard } from './render-utils.js';
 const dogListContainer = document.getElementById('dog-list-container');
 
 // on load
+async function loadData() {
+    const data = await getDogs();
+    console.log(data);
+}
+
+loadData();
 // fetch all dogs
 // render and append all dog cards to the container
